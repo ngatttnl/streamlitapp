f7d1527fd236ecf4fbd1c70e218e24971ad6ed1d
ngatttnl <62821812+ngatttnl@users.noreply.github.com>
2021-12-19 14:22:37 +0700

    Add files via upload

--- apps/binanceprice.py
+++ apps/binanceprice.py
@@ -45,15 +45,15 @@ def app():
   col1, col2, col3 = st.columns(3)
 
   # Widget (Cryptocurrency selection box)
-  col1_selection = st.sidebar.selectbox('Price 1', df.symbol, list(df.symbol).index('BTCBUSD') )
-  col2_selection = st.sidebar.selectbox('Price 2', df.symbol, list(df.symbol).index('ETHBUSD') )
-  col3_selection = st.sidebar.selectbox('Price 3', df.symbol, list(df.symbol).index('BNBBUSD') )
-  col4_selection = st.sidebar.selectbox('Price 4', df.symbol, list(df.symbol).index('XRPBUSD') )
-  col5_selection = st.sidebar.selectbox('Price 5', df.symbol, list(df.symbol).index('ADABUSD') )
-  col6_selection = st.sidebar.selectbox('Price 6', df.symbol, list(df.symbol).index('DOGEBUSD') )
-  col7_selection = st.sidebar.selectbox('Price 7', df.symbol, list(df.symbol).index('SHIBBUSD') )
-  col8_selection = st.sidebar.selectbox('Price 8', df.symbol, list(df.symbol).index('DOTBUSD') )
-  col9_selection = st.sidebar.selectbox('Price 9', df.symbol, list(df.symbol).index('MATICBUSD') )
+  col1_selection = st.sidebar.selectbox('Price 1', df.symbol, list(df.symbol).index('BTCUSDT') )
+  col2_selection = st.sidebar.selectbox('Price 2', df.symbol, list(df.symbol).index('ETHUSDT') )
+  col3_selection = st.sidebar.selectbox('Price 3', df.symbol, list(df.symbol).index('BNBUSDT') )
+  col4_selection = st.sidebar.selectbox('Price 4', df.symbol, list(df.symbol).index('XRPUSDT') )
+  col5_selection = st.sidebar.selectbox('Price 5', df.symbol, list(df.symbol).index('ADAUSDT') )
+  col6_selection = st.sidebar.selectbox('Price 6', df.symbol, list(df.symbol).index('DOGEUSDT') )
+  col7_selection = st.sidebar.selectbox('Price 7', df.symbol, list(df.symbol).index('SHIBUSDT') )
+  col8_selection = st.sidebar.selectbox('Price 8', df.symbol, list(df.symbol).index('DOTUSDT') )
+  col9_selection = st.sidebar.selectbox('Price 9', df.symbol, list(df.symbol).index('MATICUSDT') )
 
   # DataFrame of selected Cryptocurrency
   col1_df = df[df.symbol == col1_selection]
--- apps/home.py
+++ apps/home.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from binance.client import Client
+import pandas as pd
+from apps import cryptofunc
+
+client = Client()
+info = client.get_exchange_info()
 
 def app():
     st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
@@ -24,8 +30,38 @@ def app():
     </div>
   </nav>
   """, unsafe_allow_html=True)
-    st.title('Home')
 
-    st.write('This is the `home page` of this multi-page app.')
+    st.header('**Selected Price**')
 
-    st.write('In this app, we will be building a simple classification model using the Iris dataset.')
+    # Load market data from Binance API
+    df = pd.read_json('https://api.binance.com/api/v3/ticker/24hr')
+
+    # Widget (Cryptocurrency selection box) 
+    col1_selection = st.selectbox('Crypto', df.symbol, list(df.symbol).index('BTCUSDT') )
+    # Custom function for rounding values
+    def round_value(input_value):
+        if input_value.values > 1:
+            a = float(round(input_value, 2))
+        else:
+            a = float(round(input_value, 8))
+        return a
+
+    col1, col2, col3 = st.columns(3)
+
+    # DataFrame of selected Cryptocurrency
+    col1_df = df[df.symbol == col1_selection]
+
+    # Apply a custom function to conditionally round values
+    col1_price = round_value(col1_df.weightedAvgPrice)
+    
+
+    # Select the priceChangePercent column
+    col1_percent = f'{float(col1_df.priceChangePercent)}%'
+    
+    # Create a metrics price box
+    col1.metric(col1_selection, col1_price, col1_percent)
+    
+    st.header('**All Price**')
+    st.dataframe(df)
+    
+    
