724b8d435b1f32c6abd4fceb97c2bf81b3d07c4a
Tran Thi Thanh Nga <ngattt@Trans-MacBook-Pro.local>
2021-12-20 19:50:14 +0700

    commt10

--- apps/binanceprice.py
+++ apps/binanceprice.py
@@ -2,29 +2,6 @@ import streamlit as st
 import pandas as pd
 
 def app():
-  st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-
-  st.markdown("""
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-    <a class="navbar-brand" href="https://thanhnga.herokuapp.com" target="_blank">Thanh Nga</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.youtube.com/channel/UCmcQhRJ6P9kF0cvtrEdDEoQ" target="_blank">YouTube</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://twitter.com/ngattt_tb" target="_blank">Twitter</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-  """, unsafe_allow_html=True)
   st.markdown('''# **Binance Price App**
   A simple cryptocurrency price app pulling price data from *Binance API*.
   ''')
--- apps/modelper.py
+++ apps/modelper.py
@@ -6,29 +6,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score, balanced_accuracy_score, precision_score, recall_score, matthews_corrcoef, f1_score, cohen_kappa_score
 
 def app():
-    st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-
-    st.markdown("""
-  <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-    <a class="navbar-brand" href="https://thanhnga.herokuapp.com" target="_blank">Thanh Nga</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.youtube.com/channel/UCmcQhRJ6P9kF0cvtrEdDEoQ" target="_blank">YouTube</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://twitter.com/ngattt_tb" target="_blank">Twitter</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
-  """, unsafe_allow_html=True)
     # Calculates performance metrics
     def calc_metrics(input_data):
         Y_actual = input_data.iloc[:,0]
