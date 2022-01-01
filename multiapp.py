eb5ea7089310fdb3517daf94378641e9239b27a3
Tran Thi Thanh Nga <ngattt@Trans-MacBook-Pro.local>
2022-01-01 19:04:55 +0700

    commit01014

--- multiapp.py
+++ multiapp.py
@@ -52,13 +52,131 @@ class MultiApp:
         if 'key' not in st.session_state:
             st.session_state['key'] = 'en'
         st.session_state['key'] = st.sidebar.selectbox("Select language", languages.keys(), format_func=lambda x:languages[ x ])
-        ex_menu = f"""
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-            <link rel="stylesheet" href="https://github.com/ngatttnl/streamlitapp/blob/main/style.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+       
+        ex_menu = """
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <style>
+                .wrapMenuHeader,
+                .wrapMenuHeader * {
+                    margin: 0;
+                    padding: 0;
+                    box-sizing: border-box;
+                }
+                .wrapMenuHeader a {
+                    text-decoration: none;
+                }
+
+                .wrapMenuHeader {
+                    width: 100%;
+                    background-color: #3498DB;
+                    padding-left: 20px;
+                    padding-right: 20px;
+                    padding-top: 15px;
+                    padding-bottom: 15px;
+                    display: flex;
+                    align-items: center;
+                    position: relative;
+                }
+                .wrapMenuHeader .wrapLogoMenu {
+                    display: flex;
+                    align-items: center;
+                    gap: 30px;
+                    width: 100%;
+                }
+
+                .wrapMenuHeader .logoName {
+                    font-size:22px;
+                    font-weight: 700px;
+                    color: white
+                }
+
+                .wrapMenuHeader .wrapMenu {
+                    list-style: none;
+                    display: flex;
+                    gap: 20px;
+                }
+
+                .wrapMenuHeader .wrapMenu .MenuLink {
+                    color: #9ACCED;
+                    font-size: 18px;
+                    font-weight: 700px;
+                }
+
+                .wrapMenuHeader .wrapMenu .MenuLink:hover {
+                    color : rgba(255,255,255,.75)
+                }
+
+                .wrapMenuHeader .btnBurger {
+                    position: absolute;
+                    top: 10px;
+                    right: 20px;
+                    cursor: pointer;
+                    width: 40px;
+                    height: 40px;
+                    border: 2px solid #49A3DF;
+                    display: none;
+                    justify-content: center;
+                    align-items: center;
+                    color: #9ACCED;
+                    border-radius: 5px;
+                    -webkit-border-radius: 5px;
+                    -moz-border-radius: 5px;
+                    -ms-border-radius: 5px;
+                    -o-border-radius: 5px;
+                    font-size: 22px
+                }
+
+                .wrapMenuHeader .btnBurger:hover {
+                    color : rgba(255,255,255,.75)
+                }
+
+                input[id="collapseMenuMoile"]:checked ~ .wrapLogoMenu {
+                    max-height: 100vh !important;
+                    transition: all .5s;
+                    -webkit-transition: all .5s;
+                    -moz-transition: all .5s;
+                    -ms-transition: all .5s;
+                    -o-transition: all .5s;
+                }
+
+                @media screen and (max-width: 1000px) {
+                    .wrapMenuHeader .btnBurger {
+                        display: flex;
+                    }
+
+                    .wrapMenuHeader .wrapLogoMenu {
+                        display: flex;
+                        height: auto;
+                        align-items: flex-start;
+                        gap: 0px;
+                        flex-direction: column;
+                        max-height: 30px;
+                        overflow: hidden;
+                        transition: all .2s;
+                        -webkit-transition: all .2s;
+                        -moz-transition: all .2s;
+                        -ms-transition: all .2s;
+                        -o-transition: all .2s;
+                    }
+                    
+                    .wrapMenuHeader .wrapMenu {
+                        flex-direction:column;
+                        gap: 0px;
+                        width: 100%;
+                        margin-top: 10px;
+                    }
+                    
+                    .wrapMenuHeader .wrapMenu .MenuLink {
+                        width: 100%;
+                        display: flex;
+                        padding-top: 8px;
+                        padding-bottom: 8px;
+                    }
+                }
+                </style>
             <div class="wrapMenuHeader">
                 <input type="checkbox" hidden id="collapseMenuMoile">
                 <div class="wrapLogoMenu">
-                    
                     <a href="http://thanhnga.tk" class="logoName">
                         Thanh Nga
                     </a>
@@ -66,7 +184,7 @@ class MultiApp:
                     <ul class="wrapMenu">
                         <li class="menuItem">
                             <a href="http://khuonchauthanhphuc.tk" class="MenuLink">
-                                Khuôn chậu Thanh Trúc
+                                Khuôn chậu Thanh Phúc
                             </a>
                         </li>    
                         <li class="menuItem">
@@ -119,7 +237,7 @@ class MultiApp:
             </div>
             </nav>
             """
-        #st.markdown(expand_menu, unsafe_allow_html=True)
+        #st.markdown(ex_menu, unsafe_allow_html=True)
         components.html(ex_menu, height=120, scrolling=True)
          
         hide_menu_style = """
